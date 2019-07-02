225c474059fc30cbac6c7e599386755054b768cf
wtee <wtee@users.noreply.github.com>
2019-07-01 21:07:01 -0500

    Add db.py and config.py

---
+++ doiowa/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, create_engine, JSON, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from doiowa.config import DB_CONN
+
+engine = create_engine(DB_CONN)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class DoiJson(Base):
+    doi = Column(String(50), primary_key=True)
+    json = Column(JSON)
+
+
+def create_db(engine):
+    Base.metadata.create_all(engine)
