960a51c43d2b68861ca0fcdef31d9f1059eae20e
Nausicaa Rose <2069019+nausicaa-rose@users.noreply.github.com>
2026-04-08 14:20:48 -0500

    Bump version number

--- src/doiowa/__init__.py
+++ src/doiowa/__init__.py
@@ -1,7 +1,7 @@
 """A library for automating the DOI creation workflows."""
 
 __name__ = "doiowa"
-__version__ = "2024.3.22"
+__version__ = "2026.4.8"
 __author__ = "Nausicaa Rose"
 __email__ = "nausicaa@iastate.edu"
 
